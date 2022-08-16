49dd09c63e2f9ad9680380c672138a4f5590e7b0
Warnner Sinotti <warnner.sinotti@gmail.com>
2022-08-16 12:27:20 -0300

    Fonts

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -6,15 +6,20 @@ export const MyButtonSubmit = styled.TouchableOpacity`
   flex-direction: row;
   justify-content: center;
   background-color: ${(props: any) => props.theme.COLORS.secondary};
-  align-items: center;  
+  align-items: center;
   border-radius: 8px;
 `;
 
 export const MyButtonTextSubmit = styled.Text`
-  color: ${(props: any) => props.theme.COLORS.color};
+  color: ${(props: any) => props.theme.COLORS.colorTextSubmit};
   font-size: ${(props: any) => props.theme.FONT_SIZE.button};
-`
+`;
 
 export const MyByttonSocialSubmit = styled(MyButtonSubmit)`
   background-color: ${(props: any) => props.theme.COLORS.neutral};
+`;
+
+export const MyByttonSocialTextSubmit = styled(MyButtonTextSubmit)`
+  color: ${(props: any) => props.theme.COLORS.colorTextSocialButton};
+  font-size: ${(props: any) => props.theme.FONT_SIZE.button};
 `;
